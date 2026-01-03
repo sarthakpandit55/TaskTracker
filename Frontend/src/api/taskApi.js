3bf6faf19ab6861c9076b7870868c7fb05feaa5b
sarthakpandit55 <sarthakpandit55@gmail.com>
2026-01-03 14:52:02 +0530

    Initial commit - Task Tracker(MERN)

--- Frontend/src/api/taskApi.js
+++ Frontend/src/api/taskApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/tasks";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getTasks = async () => {
     const res = await fetch(BASE_URL);
